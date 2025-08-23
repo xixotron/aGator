3d738c9cc9da195aa2738afde4dcdc1c6103ddff
xixotron <xixotron89@gmail.com>
2025-08-23 18:18:10 +0200

    as per example merged login and register into user

--- handle_user.go
+++ handle_user.go
@@ -41,3 +41,23 @@ func printUser(user database.User) {
 	fmt.Printf(" * ID:      %v\n", user.ID)
 	fmt.Printf(" * Name:    %v\n", user.ID)
 }
+func handleLogin(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("Usage: %s <username>", cmd.Name)
+	}
+
+	name := cmd.Args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("couldn't find user: %w", err)
+	}
+
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %w", err)
+	}
+
+	fmt.Println("User switched successfully!")
+	return nil
+}
