7296681756dfec2a6feef5b9ac08298ee2f9d69d
xixotron <xixotron89@gmail.com>
2025-08-24 14:55:42 +0200

    split into two handlers

--- handle_rss.go
+++ handle_rss.go
@@ -3,10 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
-	"time"
 
-	"github.com/google/uuid"
-	"github.com/xixotron/aGator/internal/database"
 	"github.com/xixotron/aGator/internal/rss"
 )
 
@@ -21,74 +18,4 @@ func handleAgg(s *state, cmd command) error {
 
 	fmt.Printf("Feed: %+v\n", feed)
 	return nil
-}
-
-func handleAddFeed(s *state, cmd command) error {
-	if len(cmd.Args) != 2 {
-		return fmt.Errorf("Usage: %s <name> <url>", cmd.Name)
-	}
-	name := cmd.Args[0]
-	url := cmd.Args[1]
-
-	ctx := context.Background()
-	user, err := s.db.GetUser(ctx, s.cfg.CurrentUserName)
-	if err != nil {
-		return fmt.Errorf("couldn't find active user: %w", err)
-	}
-
-	feed, err := s.db.CreateFeed(ctx, database.CreateFeedParams{
-		ID:        uuid.New(),
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-		Name:      name,
-		Url:       url,
-		UserID:    user.ID,
-	})
-	if err != nil {
-		return fmt.Errorf("coudn't create feed: %w", err)
-	}
-
-	fmt.Println("feed added succesfully:")
-	printFeed(feed, user)
-	return nil
-}
-
-func handleListFeeds(s *state, cmd command) error {
-	ctx := context.Background()
-	feeds, err := s.db.GetFeeds(ctx)
-	if err != nil {
-		return fmt.Errorf("coudn't list feeds: %w", err)
-	}
-
-	if len(feeds) == 0 {
-		fmt.Println("No feeds found")
-		return nil
-	}
-
-	fmt.Println("Registered feeds:")
-	users := make(map[uuid.UUID]database.User)
-	for _, feed := range feeds {
-		user, ok := users[feed.UserID]
-		if !ok {
-			user, err = s.db.GetUserByID(ctx, feed.UserID)
-			if err != nil {
-				return fmt.Errorf("couldn't find username: %w", err)
-			}
-			users[feed.UserID] = user
-		}
-
-		printFeed(feed, user)
-		fmt.Println()
-		fmt.Println("=====================================")
-	}
-
-	return nil
-}
-func printFeed(f database.Feed, user database.User) {
-	fmt.Printf(" * ID:       %v\n", f.ID)
-	fmt.Printf(" * Name:     %v\n", f.Name)
-	fmt.Printf(" * Created:  %v\n", f.CreatedAt)
-	fmt.Printf(" * Updated:  %v\n", f.UpdatedAt)
-	fmt.Printf(" * URL:      %v\n", f.Url)
-	fmt.Printf(" * UserName: %v\n", f.UserID)
 }
